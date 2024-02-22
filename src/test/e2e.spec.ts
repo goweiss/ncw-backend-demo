926dd6bcb5eebbd82e5f4c93fdbb018dba895f38
Golan Weiss <gweiss@fireblocks.com>
2024-02-22 11:23:10 +0200

    Add main.ts and init function to fix db initialization race condition when running e2e tests

--- src/test/e2e.spec.ts
+++ src/test/e2e.spec.ts
@@ -23,7 +23,7 @@ import { NcwSdk } from "fireblocks-sdk/dist/src/ncw-sdk";
 import { TAssetSummary } from "../services/asset.service";
 import { mockInfoResponse } from "./mockInfoResponse";
 import { Passphrase, PassphraseLocation } from "../model/passphrase";
-import { DEFAULT_ORIGIN } from "../server";
+import { DEFAULT_ORIGIN, init } from "../server";
 import { AuthOptions } from "../middleware/jwt";
 import {
   ownedAssetsMock,
@@ -85,6 +85,8 @@ describe("e2e", () => {
   let publicKey: string, privateKey: string;
 
   beforeAll(async () => {
+    await init();
+
     const pair = await generateKeyPair("rsa", {
       modulusLength: 1024,
       publicKeyEncoding: {
@@ -161,7 +163,7 @@ describe("e2e", () => {
 
   async function getDevices() {
     return await request(app)
-      .get(`/api/devices/`)
+      .get("/api/devices/")
       .set("Authorization", `Bearer ${accessToken}`)
       .expect(200);
   }
@@ -196,7 +198,7 @@ describe("e2e", () => {
 
   async function getPassphrases() {
     return await request(app)
-      .get(`/api/passphrase/`)
+      .get("/api/passphrase/")
       .set("Authorization", `Bearer ${accessToken}`)
       .expect(200);
   }
@@ -277,7 +279,7 @@ describe("e2e", () => {
 
   async function getWallets() {
     return await request(app)
-      .get(`/api/wallets/`)
+      .get("/api/wallets/")
       .set("Authorization", `Bearer ${accessToken}`)
       .expect(200);
   }
@@ -317,7 +319,7 @@ describe("e2e", () => {
     const signature = signer.sign(privateKey, "base64");
 
     return await request(app)
-      .post(`/api/webhook`)
+      .post("/api/webhook")
       .send(payload)
       .set("fireblocks-signature", signature)
       .expect(200);
